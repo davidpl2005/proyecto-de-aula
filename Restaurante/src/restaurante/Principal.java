f06488d7a610005d2962c0df740efc11c26a7779
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-04-22 13:05:21 -0500

    Registro de usuario

--- Restaurante/src/restaurante/Principal.java
+++ Restaurante/src/restaurante/Principal.java
@@ -1,12 +1,17 @@
 
 package restaurante;
 
+import views.VentanaPrincipal;
+
 
 public class Principal {
 
     
     public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
         
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
     }
     
 }
--- Restaurante/src/views/VentanaPrincipal.java
+++ Restaurante/src/views/VentanaPrincipal.java
@@ -4,6 +4,13 @@
  */
 package views;
 
+import java.awt.Image;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+
 /**
  *
  * @author Usuario
@@ -13,8 +20,11 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     /**
      * Creates new form VentanaPrincipal
      */
+    
     public VentanaPrincipal() {
         initComponents();
+        this.setLocationRelativeTo(this);
+        SetImageLabel(etiImagen, "src/imagenes/comida italiana.jpg");
     }
 
     /**
@@ -28,77 +38,97 @@ public class VentanaPrincipal extends javax.swing.JFrame {
 
         bienvenida = new javax.swing.JLabel();
         etiNombre = new javax.swing.JLabel();
-        nombre = new javax.swing.JTextField();
-        documento = new javax.swing.JTextField();
+        nombreText = new javax.swing.JTextField();
+        documentoText = new javax.swing.JTextField();
         etiDocumento = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        btnSiguiente = new javax.swing.JButton();
+        btnGuardar = new javax.swing.JButton();
+        etiMensaje2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        textMensaje = new javax.swing.JTextArea();
+        etiImagen = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Restaurante");
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         bienvenida.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         bienvenida.setForeground(new java.awt.Color(255, 255, 255));
         bienvenida.setText("Registro del cliente");
         bienvenida.setAlignmentX(0.5F);
+        getContentPane().add(bienvenida, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 100, -1, -1));
 
         etiNombre.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         etiNombre.setForeground(new java.awt.Color(255, 255, 255));
         etiNombre.setText("Nombre completo:");
+        getContentPane().add(etiNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 150, 107, 24));
+        getContentPane().add(nombreText, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 150, 218, 31));
+        getContentPane().add(documentoText, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 200, 218, 31));
 
         etiDocumento.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         etiDocumento.setForeground(new java.awt.Color(255, 255, 255));
         etiDocumento.setText("Número documento:");
+        getContentPane().add(etiDocumento, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 126, 24));
 
-        jButton1.setBackground(new java.awt.Color(153, 255, 153));
-        jButton1.setText("Siguiente");
+        btnSiguiente.setBackground(new java.awt.Color(153, 255, 153));
+        btnSiguiente.setText("Siguiente");
+        getContentPane().add(btnSiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 290, -1, -1));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(88, 88, 88)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(108, 108, 108)
-                        .addComponent(bienvenida)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(etiNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(etiDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, 126, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(documento, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(156, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(80, 80, 80))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(98, 98, 98)
-                .addComponent(bienvenida)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(etiNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(etiDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(documento, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(47, 47, 47)
-                .addComponent(jButton1)
-                .addContainerGap(215, Short.MAX_VALUE))
-        );
+        btnGuardar.setText("Guardar");
+        btnGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 290, -1, -1));
+
+        etiMensaje2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        etiMensaje2.setForeground(new java.awt.Color(255, 255, 255));
+        getContentPane().add(etiMensaje2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 450, 198, 25));
+
+        textMensaje.setEditable(false);
+        textMensaje.setColumns(20);
+        textMensaje.setRows(5);
+        textMensaje.setBorder(null);
+        jScrollPane1.setViewportView(textMensaje);
+
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 340, 326, 92));
+        getContentPane().add(etiImagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 520, 510));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
+        String nombre = nombreText.getText();
+        String documento = documentoText.getText();
+        if(!validarCaracteres(nombre) || nombre.charAt(0)==' ' || nombre.isEmpty()){
+            JOptionPane.showMessageDialog(null,"Nombre invalido.");
+            return;
+        }
+        
+        if(!validarNumeros(documento) || documento.isEmpty()){
+            JOptionPane.showMessageDialog(null, "Número de documento invalido.");
+            return;
+        }
+        
+        textMensaje.setText("Sr(a) "+nombre+"\nEstamos muy felices de atenderlo(a) el día de hoy.\n" +"\nNumero de identificación del cliente: "+documento);
+        etiMensaje2.setText("Registro realizado con exito");
+    }//GEN-LAST:event_btnGuardarActionPerformed
+    
+    public void SetImageLabel(JLabel nombrelabel, String root){
+        ImageIcon imagen = new ImageIcon(root); 
+        Icon icon = new ImageIcon(
+                imagen.getImage().getScaledInstance(nombrelabel.getWidth(), nombrelabel.getHeight(), Image.SCALE_DEFAULT));
+        nombrelabel.setIcon(icon);
+        this.repaint();
+    }
+    public static boolean validarCaracteres(String datos){
+        return datos.matches("[a-z A-Z]*");
+    }
+    public static boolean validarNumeros(String datos){
+        return datos.matches("[0-9]*");
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -136,10 +166,15 @@ public class VentanaPrincipal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel bienvenida;
-    private javax.swing.JTextField documento;
+    private javax.swing.JButton btnGuardar;
+    private javax.swing.JButton btnSiguiente;
+    private javax.swing.JTextField documentoText;
     private javax.swing.JLabel etiDocumento;
+    private javax.swing.JLabel etiImagen;
+    private javax.swing.JLabel etiMensaje2;
     private javax.swing.JLabel etiNombre;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JTextField nombre;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField nombreText;
+    private javax.swing.JTextArea textMensaje;
     // End of variables declaration//GEN-END:variables
 }
