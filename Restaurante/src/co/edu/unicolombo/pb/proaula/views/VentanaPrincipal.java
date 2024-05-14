0b1aee0bb0f494f55c0c8394daa9ba2643c5080e
Luis <luis.martinezlopez@unicolombo.edu.co>
2024-05-14 18:34:17 -0500

    Todo perfecto

--- Restaurante/src/co/edu/unicolombo/pb/proaula/views/VentanaPrincipal.java
+++ Restaurante/src/co/edu/unicolombo/pb/proaula/views/VentanaPrincipal.java
@@ -5,18 +5,10 @@
 package co.edu.unicolombo.pb.proaula.views;
 
 import co.edu.unicolombo.pb.proaula.conceptos.Cliente;
-import java.awt.Dimension;
 import java.awt.Image;
-import java.awt.Toolkit;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import javax.swing.Icon;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -27,31 +19,23 @@ import javax.swing.JOptionPane;
  * @author Usuario
  */
 public class VentanaPrincipal extends javax.swing.JFrame {
-    
-    String[] documento;
 
 public VentanaPrincipal() {
         initComponents();
-        
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int x = (int) ((screenSize.getWidth() - this.getWidth()) / 2);
-    int y = (int) ((screenSize.getHeight() - this.getHeight()) / 2);
-
-    this.setLocation(x, y);
         btnSiguiente.setEnabled(false);
         
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-    
-    int x = (int) ((screenSize.getWidth() - this.getWidth()) / 2);
-    int y = (int) ((screenSize.getHeight() - this.getHeight()) / 2);
-
-    
-    this.setLocation(x, y);
-        btnSiguiente.setEnabled(false);
-        SetImageLabel(etiImagen, "src/imagenes/comida italiana.jpg");
         
+        this.addComponentListener(new ComponentAdapter() {
+            @Override
+    public void componentResized(ComponentEvent e) {
+       
+        ImageIcon icono = new ImageIcon("src/imagenes/comida italiana.jpg");
+        Image imagen = icono.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
         
+        // Establecer la imagen escalada en el JLabel
+        etiImagen.setIcon(new ImageIcon(imagen));
+    }
+});
     }
    
     /**
@@ -77,54 +61,37 @@ public VentanaPrincipal() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Bambino");
-        setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        bienvenida.setFont(new java.awt.Font("Microsoft JhengHei UI", 1, 28)); // NOI18N
+        bienvenida.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         bienvenida.setForeground(new java.awt.Color(255, 255, 255));
         bienvenida.setText("Registro del cliente");
         bienvenida.setAlignmentX(0.5F);
-        getContentPane().add(bienvenida, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 90, -1, -1));
-        getContentPane().add(bienvenida, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 70, -1, -1));
-        etiNombre.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        getContentPane().add(bienvenida, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 100, -1, -1));
+
+        etiNombre.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         etiNombre.setForeground(new java.awt.Color(255, 255, 255));
         etiNombre.setText("Nombre completo:");
-        getContentPane().add(etiNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 140, 107, 24));
+        getContentPane().add(etiNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 150, 107, 24));
 
         nombreText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 nombreTextActionPerformed(evt);
             }
         });
-        getContentPane().add(nombreText, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 140, 218, 31));
+        getContentPane().add(nombreText, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 150, 218, 31));
 
         documentoText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 documentoTextActionPerformed(evt);
             }
         });
-        getContentPane().add(documentoText, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 190, 218, 31));
-        getContentPane().add(etiNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 140, 160, 24));
+        getContentPane().add(documentoText, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 200, 218, 31));
 
-        nombreText.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                nombreTextActionPerformed(evt);
-            }
-        });
-        getContentPane().add(nombreText, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 140, 218, 31));
-
-        documentoText.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                documentoTextActionPerformed(evt);
-            }
-        });
-        getContentPane().add(documentoText, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 190, 218, 31));
-
-        etiDocumento.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        etiDocumento.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         etiDocumento.setForeground(new java.awt.Color(255, 255, 255));
         etiDocumento.setText("Número documento:");
-        getContentPane().add(etiDocumento, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 190, 126, 24));
-        getContentPane().add(etiDocumento, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 190, 180, 24));
+        getContentPane().add(etiDocumento, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 126, 24));
 
         btnSiguiente.setBackground(new java.awt.Color(153, 255, 153));
         btnSiguiente.setText("Siguiente");
@@ -133,9 +100,7 @@ public VentanaPrincipal() {
                 btnSiguienteActionPerformed(evt);
             }
         });
-        getContentPane().add(btnSiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 280, -1, -1));
-        getContentPane().add(btnSiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 280, -1, -1));
-
+        getContentPane().add(btnSiguiente, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 290, -1, -1));
 
         btnGuardar.setText("Guardar");
         btnGuardar.addActionListener(new java.awt.event.ActionListener() {
@@ -143,16 +108,11 @@ public VentanaPrincipal() {
                 btnGuardarActionPerformed(evt);
             }
         });
-        getContentPane().add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 280, -1, -1));
+        getContentPane().add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 290, -1, -1));
 
         etiMensaje2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         etiMensaje2.setForeground(new java.awt.Color(255, 255, 255));
-        getContentPane().add(etiMensaje2, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 440, 198, 25));
-        getContentPane().add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 280, -1, -1));
-
-        etiMensaje2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        etiMensaje2.setForeground(new java.awt.Color(255, 255, 255));
-        getContentPane().add(etiMensaje2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 440, 198, 25));
+        getContentPane().add(etiMensaje2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 450, 198, 25));
 
         textMensaje.setEditable(false);
         textMensaje.setColumns(20);
@@ -160,13 +120,8 @@ public VentanaPrincipal() {
         textMensaje.setBorder(null);
         jScrollPane1.setViewportView(textMensaje);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 330, 326, 92));
-        getContentPane().add(etiImagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 650, 560));
-
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 330, 326, 92));
-
-        etiImagen.setPreferredSize(new java.awt.Dimension(550, 400));
-        getContentPane().add(etiImagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 700, 560));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 340, 326, 92));
+        getContentPane().add(etiImagen, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 520, 510));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -186,70 +141,40 @@ public VentanaPrincipal() {
         
         textMensaje.setText("Sr(a) "+Cliente.nombre+"\nEstamos muy felices de atenderlo(a) el día de hoy.\n" +"\nNumero de identificación del cliente: "+Cliente.documento);
         etiMensaje2.setText("Registro realizado con exito");
-
-        btnSiguiente.setEnabled(true);
         
         btnSiguiente.setEnabled(true);
-        
-        try {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("Datos.txt", true));
-        writer.write("DATOS CLIENTE");
-        writer.newLine();
-        writer.write("\nNombre: " + Cliente.nombre +"\nDocumento: "+Cliente.documento+"\n");
-        writer.write("");
-        writer.newLine();
-
-        writer.close();
-    } catch (IOException e) {
-        System.err.println("Error al escribir en el archivo.");
-        e.printStackTrace();
-    }
     }//GEN-LAST:event_btnGuardarActionPerformed
 
     private void btnSiguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSiguienteActionPerformed
         
+    MenuRestaurante menu = new MenuRestaurante();
     
+            this.setVisible(false);
+        menu.setLocationRelativeTo(null);
+        menu.setVisible(true);
         
     }//GEN-LAST:event_btnSiguienteActionPerformed
 
     private void nombreTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nombreTextActionPerformed
-     documentoText.requestFocus();
-    }//GEN-LAST:event_nombreTextActionPerformed
-
-    private void documentoTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_documentoTextActionPerformed
-    btnGuardar.doClick();
         // TODO add your handling code here:
-                documentoText.requestFocus();
+        documentoText.requestFocus();
     }//GEN-LAST:event_nombreTextActionPerformed
 
     private void documentoTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_documentoTextActionPerformed
         // TODO add your handling code here:
-                btnGuardar.doClick();
+        btnGuardar.doClick();
     }//GEN-LAST:event_documentoTextActionPerformed
     
-    public void SetImageLabel(JLabel nombrelabel, String root){
-        
-    this.addComponentListener(new ComponentAdapter() {
-    @Override
-    public void componentResized(ComponentEvent e) {
-       
-        ImageIcon icono = new ImageIcon(root);
-        Image imagen = icono.getImage().getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
-        
-        
-        nombrelabel.setBounds(0,0,getWidth(), getHeight());
-        nombrelabel.setIcon(new ImageIcon(imagen));
-    }
-});
-
-    }
+    
     public static boolean validarCaracteres(String datos){
         return datos.matches("[a-z A-Z]*");
     }
     public static boolean validarNumeros(String datos){
         return datos.matches("[0-9]*");
     }
-    
+   
+// Agrega un ComponentListener a tu JFrame para detectar cambios de tamaño
+
     /**
      * @param args the command line arguments
      */
